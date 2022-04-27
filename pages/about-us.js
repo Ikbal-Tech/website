aeb03694c105ff710d93983ff4d6864a5273a6e7
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-04-27 16:39:31 +0300

    Add about thingy

--- pages/about-us.js
+++ pages/about-us.js
@@ -3,11 +3,33 @@ import React from "react";
 
 const aboutUs = () => {
   return (
-    <Stack padding="30px" justifyContent="center" alignItems="center">
+    <Stack
+      padding="30px"
+      justifyContent="center"
+      alignItems="center"
+      spacing={8}
+    >
       <Stack>
-        <Typography variant="h3">Who we are?</Typography>
-        <Typography>
+        <Typography variant="h3" textAlign="center">
+          Who we are?
+        </Typography>
+        <Typography textAlign="center">
           We are 13 student/developer who is ready to develop and create value
+        </Typography>
+      </Stack>
+
+      <Stack>
+        <Typography variant="h3" textAlign="center">
+          What are our goals?
+        </Typography>
+        <Typography textAlign="center">
+          We want to build a perfect brain device to handle color with thinking.
+        </Typography>
+      </Stack>
+
+      <Stack>
+        <Typography variant="h3" textAlign="center">
+          FOTOLAR
         </Typography>
       </Stack>
     </Stack>
