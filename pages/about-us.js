85ed6a24e99d0d2fbd0f423abce38599865cba4f
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-04-27 17:23:59 +0300

    Arrenge photos1

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,5 +1,30 @@
-import { Stack, Typography } from "@mui/material";
+import { Avatar, Stack, Typography } from "@mui/material";
 import React from "react";
+
+const TeamMember = ({ path, name }) => {
+  return (
+    <Stack
+      height="300px"
+      width="250px"
+      justifyContent="center"
+      alignItems="center"
+      spacing={1}
+      sx={{
+        borderRadius: "20px",
+        background: "#f5f5f5",
+      }}
+    >
+      <Avatar
+        src={path}
+        sx={{
+          width: "150px",
+          height: "150px",
+        }}
+      />
+      <Typography fontWeight="bold">{name}</Typography>
+    </Stack>
+  );
+};
 
 const aboutUs = () => {
   return (
@@ -29,8 +54,26 @@ const aboutUs = () => {
 
       <Stack>
         <Typography variant="h3" textAlign="center">
-          FOTOLAR
+          Our Team
         </Typography>
+
+        <Stack
+          flexDirection="row"
+          flexWrap="wrap"
+          margin="0 auto"
+          gap={2}
+          sx={{
+            width: "60%",
+          }}
+        >
+          <TeamMember path="/images/team/Abdullah_Yigit.jpeg" name="domates" />
+          <TeamMember path="/images/team/Abdullah_Yigit.jpeg" name="domates" />
+          <TeamMember path="/images/team/Abdullah_Yigit.jpeg" name="domates" />
+          <TeamMember path="/images/team/Abdullah_Yigit.jpeg" name="domates" />
+          <TeamMember path="/images/team/Abdullah_Yigit.jpeg" name="domates" />
+          <TeamMember path="/images/team/Abdullah_Yigit.jpeg" name="domates" />
+          <TeamMember path="/images/team/Abdullah_Yigit.jpeg" name="domates" />
+        </Stack>
       </Stack>
     </Stack>
   );
