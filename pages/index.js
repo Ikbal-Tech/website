0876774f9b37223f99aab08709a9ddc0d78470bb
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-04-27 17:31:32 +0300

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import Navbar from "../components/Navbar/AppBar";
 
 const index = () => {
   return (
-    <Stack padding="30px">
-      <Typography textAlign="center">Ikbal Tech Home</Typography>
+    <Stack
+      padding="30px"
+      spacing={5}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Typography variant="h1" textAlign="center">
+        Ikbal Tech Home
+      </Typography>
+      <Button
+        textAlign="center"
+        href="/about-us"
+        sx={{
+          width: "50%",
+        }}
+      >
+        <Typography variant="h3" textAlign="center">
+          You can check our team from About Us Page
+        </Typography>
+      </Button>
+      <Button
+        textAlign="center"
+        href="timeline"
+        sx={{
+          width: "50%",
+        }}
+      >
+        <Typography variant="h3" textAlign="center">
+          You can check our progress from Timeline Page
+        </Typography>
+      </Button>
     </Stack>
   );
 };
